d3ec75e3412d1d863b1d0cd152450368373136f0
fdesimini <fdesimini@users.noreply.github.com>
2015-05-25 19:28:15 -0400

    added FB and Parse but connection not working

---
+++ LikeMeApp/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  LikeMeApp
+//
+//  Created by Frank Desimini on 2015-05-25.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#ifndef LikeMeApp_Bridging_Header_h
+#define LikeMeApp_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif
--- LikeMeApp/ViewController.swift
+++ LikeMeApp/ViewController.swift
@@ -7,14 +7,87 @@
 //
 
 import UIKit
+import Parse
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        
+//        let testObject = PFObject(className: "TestObject")
+//        testObject["foo"] = "bar"
+//        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+//            println("Object has been saved.")
+//        }
+        
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+        
     }
 
+    // Facebook Delegate Methods - This is helpful to know if the user did login correctly and if they did you can grab their information.
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
+    }
+    
+//  method to grab the Users Facebook data. You can call this method anytime after a user has logged in by calling self.returnUserData().
+    
+//    func returnUserData()
+//    {
+//        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+//        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+//            
+//            if ((error) != nil)
+//            {
+//                // Process error
+//                println("Error: \(error)")
+//            }
+//            else
+//            {
+//                println("fetched user: \(result)")
+//                let userName : NSString = result.valueForKey("name") as! NSString
+//                println("User Name is: \(userName)")
+//                let userEmail : NSString = result.valueForKey("email") as! NSString
+//                println("User Email is: \(userEmail)")
+//            }
+//        })
+//    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
