89af2768a0b43ea26d0d03c34e5cefc30d8774b9
fdesimini <fdesimini@users.noreply.github.com>
2015-05-25 15:29:13 -0400

    Initial Commit

---
+++ LikeMeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LikeMeApp
+//
+//  Created by Frank Desimini on 2015-05-25.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
